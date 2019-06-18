f350a65385de555f7a8ad715ee4de265c4655539
William Homer <billyhomeriii@gmail.com>
2019-06-18 11:07:14 -0500

    change items index,show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :items, only: [:index, :show]
+  # resources :items, only: [:index, :show]
+  get '/items', to: 'items#index', as: :items
+  get '/items/:id', to: 'items#show', as: :item
 
   get '/carts', to: 'carts#show'
   post '/carts', to: 'carts#add'
